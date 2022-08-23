9735b8d66792f048c878d0a667279e704d504db6
Alex <olivesargreek@gmail.com>
2022-08-23 13:29:58 +0300

    fix: Auth component

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,8 +1,29 @@
+import React from 'react';
 import style from './Auth.module.css';
-import Button from './Button /Button';
+import {ReactComponent as AuthIcon} from './img/login.svg';
+import {urlAuth} from '../../../api/auth';
+import {PropTypes} from 'prop-types';
+import {useAuth} from '../../../hooks/useAuth';
 
-export const Auth = () => (
-  <div className={style.user}>
-    <Button />
-  </div>
-);
+
+export const Auth = () => {
+  const [auth] = useAuth();
+  return (
+    <>
+      {auth.username ? (
+        <div className={style.profile_info}>
+          <img src={auth['profile_image'].small}/>
+          <span>{auth.username}</span>
+        </div>
+      ) : (
+        <a href={urlAuth} className={style.button}>
+          <AuthIcon className={style.icon}/>
+        </a>
+      )}
+    </>
+  );
+};
+
+Auth.propTypes = {
+  token: PropTypes.string,
+};
